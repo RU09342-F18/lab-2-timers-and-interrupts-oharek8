6d67299acdfd9517659b407775c8195e0d3eaa7b
Kevin Ohare <oharek8@students.rowan.edu>
2018-09-22 16:32:11 -0400

    Added Timer A for G2553

---
+++ TIMER A Blink/MSP430G2553/main.c
@@ -0,0 +1,24 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P1DIR |= BIT0;                     // Set pin 1.0 to output
+	CCTL0 = CCIE;                      // Enable CCR0 interrupt
+	TACTL = TASSEL_2 + MC_1 + ID_3;    // Set Timer A to use smclk/8 on up mode
+	CCR0 = 50000;                      // Set CCR0 to 50000
+	__bis_SR_register(LPM0_bits + GIE);
+
+	return 0;
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+    P1OUT ^= BIT0;                     // Toggle LED at timer interrupt
+}
