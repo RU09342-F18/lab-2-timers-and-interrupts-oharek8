00b374875566489108c49f20b461f031d75977bb
Kevin Ohare <oharek8@students.rowan.edu>
2018-09-27 10:35:54 -0400

    Edited documentation

--- Button Based Delay/MSP430G2553/main.c
+++ Button Based Delay/MSP430G2553/main.c
@@ -7,20 +7,24 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+    //LED Setup
 	P1DIR |= BIT0;                     // Set pin 1.0 to output
 
+    //Button 1
 	P1DIR &= ~BIT3;                    // Set pin 1.3 to input
 	P1IE |=  BIT3;                     // Enable interupt on pin 1.3
 	P1IES |= BIT3;                     // Detect falling edge
     P1REN &= ~BIT3;                     // Disable pullup
 	P1IFG &= ~BIT3;                    // Clear Flag
 
+    //Button 2
 	P2DIR &= ~BIT5;                    // Set pin 2.5 to input
 	P2IE |=  BIT5;                     // Enable interupt on pin 2.5
 	P2IES |= BIT5;                     // Detect falling edge
 	P2REN &= ~BIT5;                     // Disable pullup
 	P2IFG &= ~BIT5;                    // Clear Flag
 
+    //Timer A Setup
 	CCTL0 = CCIE;                      // Enable CCR0 interrupt
 	TACTL = TASSEL_1 + MC_1 + ID_3;    // Set Timer A to use aclk/8 on up mode
 	TACCR0 = 400;
